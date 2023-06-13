83d3f003457bc77fc40264a1b3db703204195bc3
artmadeit <this.arthur.email@gmail.com>
2023-06-12 21:09:56 -0500

    feat: initial commit

---
+++ src/test/java/pe/edu/cibertec/boletas/BoletasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.boletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoletasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
